c84466ffa6309b28593273f053a8e5b242b34826
Sandhu200 <sandhutalwinder36@gmail.com>
2024-05-07 10:57:00 +0545

    Added nav bar

--- src/data.js
+++ src/data.js
@@ -9,29 +9,24 @@ export const pageLinks = [
   { id: 3, href: '#services', text: 'services' },
   { id: 4, href: '#tours', text: 'tours' },
 ];
-export const socialLinks = [
-  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
-  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
-  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
-];
 
 export const services = [
   {
     id: 1,
     icon: 'fas fa-wallet fa-fw',
-    title: 'saving money',
+    title: 'Fun Travel',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
   },
   {
     id: 2,
     icon: 'fas fa-tree fa-fw',
-    title: 'endless hiking',
+    title: 'Hiking',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
   },
   {
     id: 3,
     icon: 'fas fa-socks fa-fw',
-    title: 'amazing comfort',
+    title: 'Confort and fun',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
   },
 ];
