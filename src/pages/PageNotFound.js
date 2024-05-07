07de6987ecfeac408bf369e5ac5f9350b454a62d
Sandhu200 <sandhutalwinder36@gmail.com>
2024-05-07 12:21:55 +0545

    Added services and page not found.

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,9 @@
+import Title from "../components/Title";
+
+const PageNotFound = () => {
+    return <>
+        <Title title="Page Not" subtitle="found" />
+    </>;
+  };
+  
+  export default PageNotFound;
---
+++ src/pages/ServicesPage.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+import Services from '../components/Services';
+import Footer from '../components/Footer';
+
+const ServicesPage = () => {
+    return <>
+      <Navbar />
+      <Services />
+      <Footer />
+    </>;
+  };
+  
+  export default ServicesPage;
