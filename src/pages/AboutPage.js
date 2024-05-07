165cb4b4034ee60cc56947e4d39742d768ba6a5e
Sandhu200 <sandhutalwinder36@gmail.com>
2024-05-07 12:05:22 +0545

    Added about page

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import About from '../components/About';
+
+const AboutPage = () => {
+    return <>
+        <Navbar />
+        <About />
+        <Footer />
+    </>;
+  };
+  
+  export default AboutPage;
