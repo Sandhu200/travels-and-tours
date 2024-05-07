830fa7965b8aa625bf2d00ecb16da942c7d9e591
Sandhu200 <sandhutalwinder36@gmail.com>
2024-05-07 11:56:17 +0545

    Added router and create pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Tours from '../components/Tours';
+import Footer from '../components/Footer';
+
+const HomePage = () => {
+    return <>
+        <Navbar />
+        <Hero />
+        <Tours />
+        <Footer />
+    </>;
+  };
+  
+  export default HomePage;
---
+++ src/pages/ToursPage.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Tours from '../components/Tours';
+import Footer from '../components/Footer';
+
+const ToursPage = () => {
+    return <>
+      <Navbar />
+      <Tours />
+      <Footer />
+    </>;
+  };
+  
+  export default ToursPage;
